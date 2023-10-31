35b2facbf2947a391907168caa9530644307b4ed
Jlouisa <jonathan.louisa@gmail.com>
2023-10-31 19:46:33 +0100

    Add boilerplate and npm

---
+++ app.js
@@ -0,0 +1,60 @@
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const app = express();
+
+//! MongoDB setup
+const mongoInit = process.env.MONGODB_INIT;
+const cluster = process.env.MONGODB_CLUSTER;
+const clusterID = process.env.MONGODB_CLUSTERID;
+const database = process.env.MONGODB_DB;
+const host = process.env.MONGODB_HOST;
+const user = encodeURIComponent(process.env.MONGODB_USER);
+const pass = encodeURIComponent(process.env.MONGODB_PASS);
+
+const mongoDB = process.env.MONGODB_URI || `${mongoInit}${user}:${pass}@${cluster}${clusterID}${database}${host}`;
+
+async function main(db) {
+  await mongoose.connect(db);
+}
+// MonogDB init connection
+main(mongoDB).catch((err) => console.log(err));
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
